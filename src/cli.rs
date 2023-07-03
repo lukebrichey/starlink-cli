187b03c9a7c929d801e368f748abef3077814a3b
lukebrichey <lukebrichey@gmail.com>
2023-07-03 13:05:31 -0400

    refactor: Refactored print_ functions into singular print_option, updated main to comply

--- src/cli.rs
+++ src/cli.rs
@@ -8,84 +8,101 @@ use tokio::sync::oneshot;
 use tokio::time::Duration;
 
 #[derive(StructOpt, Debug)]
+pub enum CliOption {
+    /// Display alerts
+    Alerts,
+    /// Display device info
+    Info,
+    /// Display obstruction stats
+    Obstruction,
+    /// Display average speed
+    Speed,
+    /// Display device state
+    State,
+}
+
+
+#[derive(StructOpt, Debug)]
 #[structopt(
     name = "starlink-cli",
-    about = "A CLI tool to interface with your Starlink dish."
+    about = "A CLI tool to interact with your Starlink dish."
 )]
 pub struct Opt {
-    /// Returns the alerts of dish
-    #[structopt(short, long)]
-    pub alerts: bool,
-
-    /// Returns the dish's device info (id, hardware/software versions, and country code)
-    #[structopt(short, long)]
-    pub info: bool,
-
-    /// Returns whether dish is currently obstructed and obstruction %
-    #[structopt(short, long)]
-    pub obstruction: bool,
-
-    /// Returns download and upload speed in mbps
-    #[structopt(short, long)]
-    pub speed: bool,
-
-    /// Returns the state of dish
-    #[structopt(long)]
-    pub state: bool,
+    /// The option chosen by the user
+    #[structopt(subcommand)]
+    pub option: CliOption,
 }
 
-pub fn print_alerts(get_status_res: &Response) {
+// Matches subcommand with response and prints the result
+pub fn print_option(option: CliOption, get_status_res: &Response) {
     if let Some(response::Response::DishGetStatus(response)) = &get_status_res.response {
-        if let Some(alerts) = &response.alerts {
-            print_colored("Motors stuck: ", &alerts.motors_stuck);
-            print_colored("thermal_throttle: ", &alerts.thermal_throttle);
-            print_colored("thermal_shutdown: ", &alerts.thermal_shutdown);
-            print_colored("mast_not_near_vertical: ", &alerts.mast_not_near_vertical);
-            print_colored("unexpected_location: ", &alerts.unexpected_location);
-            print_colored("slow_ethernet_speeds: ", &alerts.slow_ethernet_speeds);
-        }
-    }
-}
-
-// Prints device info (id, hardware/software versions, and country code)
-pub fn print_info(get_status_res: &Response) {
-    if let Some(response::Response::DishGetStatus(r)) = &get_status_res.response {
-        if let Some(device_info) = &r.device_info {
-            if let Some(id) = &device_info.id {
-                println!("Device id: {}", id)
-            }
-            if let Some(hardware_version) = &device_info.hardware_version {
-                println!("Hardware version: {}", hardware_version)
-            }
-            if let Some(software_version) = &device_info.software_version {
-                println!("Software version: {}", software_version)                
-            }
-            if let Some(country_code) = &device_info.country_code {
-                println!("Country code: {}", country_code)
-            }
-        }
-    }
-}
-
-// Prints obstruction percentage, colors based on advice from
-// https://www.starlinkhardware.com/starlink-obstructions-how-much-is-too-much/
-pub fn print_obstruction(get_status_res: &Response) {
-    if let Some(response::Response::DishGetStatus(response)) = &get_status_res.response {
-        if let Some(obstructions_stats) = &response.obstruction_stats {
-            print_colored(
-                "Currently obstructed",
-                &obstructions_stats.currently_obstructed,
-            );
-            match &obstructions_stats.fraction_obstructed {
-                Some(percentage) => {
-                    print!("Percentage obstructed: ");
-                    print_color_percentage(percentage * 100.0)
+        match option {
+            CliOption::Alerts => {
+                if let Some(alerts) = &response.alerts {
+                    print_colored("Motors stuck: ", &alerts.motors_stuck);
+                    print_colored("thermal_throttle: ", &alerts.thermal_throttle);
+                    print_colored("thermal_shutdown: ", &alerts.thermal_shutdown);
+                    print_colored("mast_not_near_vertical: ", &alerts.mast_not_near_vertical);
+                    print_colored("unexpected_location: ", &alerts.unexpected_location);
+                    print_colored("slow_ethernet_speeds: ", &alerts.slow_ethernet_speeds);
                 }
-                None => print_colored::<f32>("Percentage obstructed: ", &None),
             }
+            CliOption::Info => {
+                if let Some(device_info) = &response.device_info {
+                    if let Some(id) = &device_info.id {
+                        println!("Device id: {}", id)
+                    }
+                    if let Some(hardware_version) = &device_info.hardware_version {
+                        println!("Hardware version: {}", hardware_version)
+                    }
+                    if let Some(software_version) = &device_info.software_version {
+                        println!("Software version: {}", software_version)                
+                    }
+                    if let Some(country_code) = &device_info.country_code {
+                        println!("Country code: {}", country_code)
+                    }
+                }
+            }
+            CliOption::Obstruction => {
+                if let Some(obstructions_stats) = &response.obstruction_stats {
+                    print_colored(
+                        "Currently obstructed",
+                        &obstructions_stats.currently_obstructed,
+                    );
+                    match &obstructions_stats.fraction_obstructed {
+                        Some(percentage) => {
+                            print!("Percentage obstructed: ");
+                            print_color_percentage(percentage * 100.0)
+                        }
+                        None => print_colored::<f32>("Percentage obstructed: ", &None),
+                    }
+                }
+            }
+            CliOption::State => {
+                if let Some(device_state) = &response.device_state {
+                    match device_state.uptime_s {
+                        Some(uptime) => {
+                            let uptime = uptime as u64;
+
+                            let months = uptime / (30 * 24 * 60 * 60);
+                            let weeks = (uptime % (30 * 24 * 60 * 60)) / (7 * 24 * 60 * 60);
+                            let days = (uptime % (7 * 24 * 60 * 60)) / (24 * 60 * 60);
+                            let seconds = uptime % (24 * 60 * 60);
+                            println!(
+                                "Uptime: {} months, {} weeks, {} days, {} hours, {} minutes, {} seconds",
+                                months, weeks, days, seconds / 3600, (seconds / 60) % 60, seconds % 60
+                            );
+                        },
+                        None => print_colored::<u64>("Uptime: ", &None),
+                    }
+
+                }
+            }
+            _ => {}
         }
     }
 }
+
 
 // Calculates average speed by throwing out outliers, and then averaging the rest
 pub async fn print_speeds() {
@@ -127,28 +144,4 @@ pub async fn print_speeds() {
         avg_up,
         color::Fg(color::Reset)
     );
-}
-
-// Prints device uptime
-pub fn print_state(get_status_res: &Response) {
-    if let Some(response::Response::DishGetStatus(response)) = &get_status_res.response {
-        if let Some(device_state) = &response.device_state {
-            match device_state.uptime_s {
-                Some(uptime) => {
-                    let uptime = uptime as u64;
-
-                    let months = uptime / (30 * 24 * 60 * 60);
-                    let weeks = (uptime % (30 * 24 * 60 * 60)) / (7 * 24 * 60 * 60);
-                    let days = (uptime % (7 * 24 * 60 * 60)) / (24 * 60 * 60);
-                    let seconds = uptime % (24 * 60 * 60);
-
-                    println!(
-                        "Uptime: {} months, {} weeks, {} days, {} seconds",
-                        months, weeks, days, seconds
-                    );
-                }
-                None => println!("Uptime (s): {}", 0.0),
-            }
-        }
-    }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,36 +4,29 @@ mod helpers;
 
 use structopt::StructOpt;
 use dish::make_request;
-use cli::Opt;
+use cli::{CliOption, Opt, print_option};
 
 fn main() {
     let opt: Opt = Opt::from_args();
     let rt = tokio::runtime::Runtime::new().unwrap();
 
-    // If speed flag passed, we create a stream
-    if opt.speed {
-        rt.block_on(cli::print_speeds());
-        return;
-    }
+    match opt.option {
+        CliOption::Speed => {
+            rt.block_on(cli::print_speeds());
+        },
+        _ => {
+            let res = rt.block_on(make_request());
 
-    let res = rt.block_on(make_request());
+            // Handle errors
+            if res.is_err() {
+                println!("Error: {:?}", res.err());
+                return;
+            }
 
-    // Handle errors
-    if res.is_err() {
-        println!("Error: {:?}", res.err());
-        return;
-    }
+            // Transform response
+            let get_status_res = res.unwrap().into_inner();
 
-    // Transform response
-    let get_status_res = res.unwrap().into_inner();
-
-    if opt.alerts {
-        cli::print_alerts(&get_status_res);
-    } else if opt.obstruction {
-        cli::print_obstruction(&get_status_res);
-    } else if opt.state {
-        cli::print_state(&get_status_res);
-    } else if opt.info {
-        cli::print_info(&get_status_res);
+            print_option(opt.option, &get_status_res);
+        }
     }
 }
